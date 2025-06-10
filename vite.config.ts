9cf100082c651d48fd419e8efa6a92b91bd32de9
lucasroqe <alvim.lucas2@hotmail.com>
2025-06-09 22:09:04 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,14 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+    react()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   base: "/port-fatec/"
-}));
+});
